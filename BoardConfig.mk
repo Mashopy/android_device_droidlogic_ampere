f4e83ecee4deafcb23d6a5448884739efdeb51d1
Mashopy <eliasgheeraert@gmail.com>
2025-02-16 15:51:12 +0100

    ampere: Switch to SouthSV 6XXX WLAN drivers

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -20,19 +20,23 @@ BOARD_KERNEL_CMDLINE += androidboot.boot_devices=d0074000.emmc
 
 ## Kernel modules
 TARGET_KERNEL_EXT_MODULES := \
-    rtl8822bs:kbuild
+    ssv6xxx/ssv6051:kbuild \
+    ssv6xxx/ssv6x5x:kbuild \
+    ssv6xxx/ssv_hwif_ctrl:kbuild
 
 ## Properties
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
 ## Wi-Fi
-BOARD_WLAN_DEVICE := realtek
+BOARD_WLAN_DEVICE := amlogic
+MULTI_WIFI_SUPPORT := true
 BOARD_WPA_SUPPLICANT_DRIVER := NL80211
 WPA_SUPPLICANT_VERSION := VER_0_8_X
-BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_rtl
-BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_rtl
+BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_multi
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_multi
 WIFI_DRIVER_SOCKET_IFACE := wlan0
-PRODUCT_CFI_INCLUDE_PATHS += hardware/realtek/wlan/wpa_supplicant_8_lib
+PRODUCT_CFI_INCLUDE_PATHS += hardware/amlogic/wifi/multi_wifi/wpa_supplicant_8_lib
+PRODUCT_CFI_INCLUDE_PATHS += hardware/amlogic/wifi/wifi_hal/wpa_supplicant_8_lib
 
 ## Include the common tree BoardConfig makefile
 include device/amlogic/gx-common/BoardConfigCommon.mk
